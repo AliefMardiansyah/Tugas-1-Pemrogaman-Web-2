1d469160ed9c4a5ca02cb2c546a4cceecfbae3b7
AliefMardiansyah <148052521+AliefMardiansyah@users.noreply.github.com>
2023-10-22 15:08:26 +0700

    Add files via upload

---
+++ TugasPR.php
@@ -0,0 +1,15 @@
+<?php
+$angka = 10; // Variabel berisi angka 10
+
+// Operasi matematika
+$hasilKali = $angka * 8;
+$hasilBagi = $hasilKali / 4;
+$hasilKurang = $hasilBagi - 6;
+$hasilTambah = $hasilKurang + 2;
+
+echo "Aku adalah " . $angka . ". Jika aku dikalikan 8, jumlahku sekarang " . $hasilKali . ".<br>";
+echo "Jika aku dibagi 4, jumlahku sekarang " . $hasilBagi . ".<br>";
+echo "Jika aku dikurangi 6, jumlahku sekarang " . $hasilKurang . ".<br>";
+echo "Jika aku ditambahkan 2, jumlahku sekarang " . $hasilTambah . ".<br>";
+?>
+
